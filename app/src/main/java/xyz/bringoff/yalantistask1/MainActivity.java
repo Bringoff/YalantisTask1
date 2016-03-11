1b99c4fbe4ee60bfbed876f1d9dff254a339f8a8
Artem Kalachyan <ua.bringoff@gmail.com>
2016-03-11 19:48:45 +0200

    Home button displays as up and finishes the activity

--- app/src/main/java/xyz/bringoff/yalantistask1/MainActivity.java
+++ app/src/main/java/xyz/bringoff/yalantistask1/MainActivity.java
@@ -2,6 +2,7 @@ package xyz.bringoff.yalantistask1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
