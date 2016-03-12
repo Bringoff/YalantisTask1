cf6b16cc6ba2cfe81b43fb5c6c3d2cbeeb13b04b
Artem Kalachyan <ua.bringoff@gmail.com>
2016-03-12 09:12:15 +0200

    Custom view for title-value displaying created and added to the layout; necessary string resources added

---
+++ app/src/main/java/xyz/bringoff/yalantistask1/view/CaptionValueView.java
@@ -0,0 +1,83 @@
+package xyz.bringoff.yalantistask1.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import xyz.bringoff.yalantistask1.R;
+
+public class CaptionValueView extends LinearLayout {
+    private TextView mTitleTextView;
+    private TextView mValueTextView;
+
+    private String mCaption;
+    private String mValue;
+
+    public CaptionValueView(Context context) {
+        super(context);
+        initViews(context);
+    }
+
+    public CaptionValueView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        obtainAttributes(context, attrs);
+
+        initViews(context);
+    }
+
+    public CaptionValueView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        obtainAttributes(context, attrs);
+
+        initViews(context);
+    }
+
+    private void obtainAttributes(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CaptionValueView, 0, 0);
+        try {
+            mCaption = typedArray.getString(R.styleable.CaptionValueView_caption);
+            mValue = typedArray.getString(R.styleable.CaptionValueView_value);
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    private void initViews(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.titlevalue_view, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        mTitleTextView = (TextView) this.findViewById(R.id.title_text_view);
+        mValueTextView = (TextView) this.findViewById(R.id.value_text_view);
+
+        setCaption(mCaption);
+        setValue(mValue);
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String text) {
+        mCaption = text;
+        mTitleTextView.setText(mCaption);
+    }
+
+    public String getValue() {
+        return mValue;
+    }
+
+    public void setValue(String text) {
+        mValue = text;
+        mValueTextView.setText(mValue);
+    }
+}
