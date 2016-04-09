35e4cc05362c5a121bbcfaf3705ad7cdda11fa2f
Artem Kalachyan <ua.bringoff@gmail.com>
2016-04-09 17:31:51 +0300

    List item for requests created

---
+++ app/src/main/java/xyz/bringoff/yalantistask1/requests/RequestListActivity.java
@@ -0,0 +1,15 @@
+package xyz.bringoff.yalantistask1.requests;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xyz.bringoff.yalantistask1.R;
+
+public class RequestListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_list);
+    }
+}
