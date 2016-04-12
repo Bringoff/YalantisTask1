256cb55b6e3f1d99eaa6fb11ad0c2e9325368e4e
Artem Kalachyan <ua.bringoff@gmail.com>
2016-04-12 08:23:37 +0300

    Dates difference calculation fix

--- app/src/main/java/xyz/bringoff/yalantistask1/utils/DateUtils.java
+++ app/src/main/java/xyz/bringoff/yalantistask1/utils/DateUtils.java
@@ -9,13 +9,15 @@ import xyz.bringoff.yalantistask1.R;
 
 public class DateUtils {
 
+    public static final int MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+
     public static String unixToMediumDateString(Context context, long unixDate) {
         return DateFormat.getMediumDateFormat(context).format(new Date(unixDate));
     }
 
     public static String unixToDateDiffString(Context context, long unixDate) {
         long diff = System.currentTimeMillis() - unixDate;
-        int daysDiff = (int) (diff / 60 / 60 / 24);
+        int daysDiff = (int) (diff / MILLISECONDS_IN_DAY);
         return context.getString(
                 R.string.days_gone_after_creation_format,
                 daysDiff,
