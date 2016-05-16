f6f7df713b74317def7765a5383ea83a4f558e76
Artem Kalachyan <ua.bringoff@gmail.com>
2016-05-16 12:53:21 +0300

    ButterKnife integrated; supports version updated

--- app/src/main/java/xyz/bringoff/yalantistask1/ui/base/BaseFragment.java
+++ app/src/main/java/xyz/bringoff/yalantistask1/ui/base/BaseFragment.java
@@ -1,9 +1,14 @@
 package xyz.bringoff.yalantistask1.ui.base;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import rx.subscriptions.CompositeSubscription;
 
 public abstract class BaseFragment extends Fragment {
@@ -20,6 +25,17 @@ public abstract class BaseFragment extends Fragment {
 
         mCompositeSubscription = new CompositeSubscription();
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @LayoutRes
+    public abstract int getLayoutId();
 
     @Override
     public void onDestroy() {
